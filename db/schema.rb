00d1bfc27b2ab2db31d4907cd9e9a2ce26d9e2a9
Jamie Parsons <jhelenparsons@gmail.com>
2019-10-20 17:46:37 -0600

    Revert "changes to views, forms"

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20190929231953) do
     t.string "title"
     t.text "description"
     t.string "image_url"
-    t.decimal "price", precision: 8, scale: 2
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
