5d08922ed3826d9daf26ef984468b3c805e3bf4a
Jamie Parsons <jhelenparsons@gmail.com>
2019-10-19 21:41:19 -0600

    changes to views, forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@
 Product.delete_all
 Product.create!(title: 'CoffeeScript',
   description: 
-    %{<p>
+    %{<p>  
         CoffeeScript is JavaScript done right. It provides all of JavaScript's
 	functionality wrapped in a cleaner, more succinct syntax. In the first
 	book on this exciting new language, CoffeeScript guru Trevor Burnham
 	shows you how to hold onto all the power and flexibility of JavaScript
 	while writing clearer, cleaner, and safer code.
       </p>},
-  image_url:   'cs.jpg',    
+  image_url:   'cs.jpg',   
   price: 36.00)
 # . . .
 Product.create!(title: 'Programming Ruby 1.9 & 2.0',
