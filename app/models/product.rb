196131d22c9726199b55b918210172488680382f
Jamie Parsons <jhelenparsons@gmail.com>
2019-10-23 17:25:16 -0600

    typo in product model regex

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ validates :title, :description, :image_url, :price, presence: true
 validates :title, uniqueness: true
 validates :price, :numericality => {:greater_than => 0.01}
 validates :image_url, allow_blank: true, format: {
-    with:   %r{\.(gif|jpg|png)Z}i,
+    with:   %r{\.(gif|jpg|png)\Z}i,
     message: "must be gif, jpg, png"
 }
 end
